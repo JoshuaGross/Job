04b68e295e4384d440e03a60b073226e06de4553
Joshua Gross <joshua.gross@gmail.com>
2013-03-08 18:00:16 -0600

    First unit tests for Job.

---
+++ test/delay.js
@@ -0,0 +1,40 @@
+// This is a set of tests for our additions to the database layer
+var job = require('../')
+  , expect = require('expect.js');
+
+var emailModule = {
+  exports: { }
+};
+emailModule.filename = 'email.js';
+emailModule.exports.name = 'mailLib';
+emailModule.exports.emailsSent = 0;
+emailModule.exports.sendMail = function sendMail (email, subject, message) {
+  console.log('processing email')
+  emailModule.exports.emailsSent++;
+};
+emailModule.exports.sendMail.delayable = true;
+
+describe('job module', function () {
+  it('should allow us to register helpers', function (done) {
+    job.addHelper(emailModule);
+    process.nextTick(done);
+  });
+  it('should allow us to initialize Job', function (done) {
+    job.init();
+    done();
+  });
+  it('should allow us to delay jobs', function (done) {
+    emailModule.exports.delay.sendMail(1, 'critical', 'josh@spandex.io', 'Hello', 'world');
+    done();
+  });
+  it('should allow us to start the jobs processor', function (done) {
+    emailModule.exports.sendMail.process();
+    done();
+  });
+  it('should have processed our mail job', function (done) {
+    setTimeout(function () {
+      expect(emailModule.exports.emailsSent).to.be(1);
+      done();
+    }, 50);
+  });
+});
